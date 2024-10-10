ae06273e0007ffbef77efb88b12b854f8351250f
Amal T Scaria <amaltscaria22@gmail.com>
2024-10-10 11:08:33 +0530

    Fix font of date

--- src/utils/shareJPEG.js
+++ src/utils/shareJPEG.js
@@ -81,7 +81,7 @@ export const shareJPEG = async (name) => {
 
         // Add Date and Certificate Number
         ctx.font = "600 70px Outfit";
-        ctx.fillText(date.toUpperCase(), 300, 2540); // Adjust position
+        ctx.fillText(date, 300, 2540); // Adjust position
         ctx.font = "normal 60px Outfit";
         ctx.fillText(id, 300, 2640); // Adjust position
 
